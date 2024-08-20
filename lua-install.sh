c86815d084f67e514de4908968b25fef4d157f75
rangapv <rangapv@yahoo.com>
2024-08-20 10:40:09 +0530

    Aesthetics and bug fix

--- lua-install.sh
+++ lua-install.sh
@@ -6,7 +6,7 @@ chk_lua() {
 l1=`which lua`
 l1s="$?"
 
-if [ $l1s -eq "0" ]
+if [ $l1s -ne "0" ]
 then
 echo "lua is installed in $l1 and the version is $(lua -v)"
 else
@@ -22,24 +22,30 @@ luaver="lua-5.4.7"
 echo "The current version of lua to be installed is $luaver, check the website https://www.lua.org/download.html to confirm the release version or press enter"
 read luver
 
-if [[ $luver = "" ]]
+if [[ $luver != "" ]]
 then
 	luaver=$luver
 fi
 
-
-ldw=`curl -L -R -O https://www.lua.org/ftp/$(luaver).tar.gz`
-lu1=`tar zxf $(luaver).tar.gz`
-lu2=`cd $(luaver)`
-lu3=`sudo make all test`
-
-lu4=`sudo make install`
-
+ldw=`curl -L -R -O https://www.lua.org/ftp/$luaver.tar.gz`
+ldws="$?"
+if  [[ $ldws = "0" ]]
+then
+lu1=`tar zxf $luaver.tar.gz`
+lu2=`cd $luaver;make all test`
+lu2s="$?"
+echo "lua test passed $lu2s"
+lu4=`cd $luaver;make install`
+u4s="$?"
 luas=`lua -v`
-
 echo "Current lua is $luas"
 
-}
+else
 
+echo "Download failed "
+
+fi
+
+}
 
 chk_lua
